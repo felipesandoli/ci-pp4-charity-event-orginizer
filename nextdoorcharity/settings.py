ce2b75683136f458cb27388409f2dd8077db8cc8
felipesandoli <cfelipesandoli@gmail.com>
2023-04-13 21:04:39 +0100

    Setup templates and add base.html main structure

--- nextdoorcharity/settings.py
+++ nextdoorcharity/settings.py
@@ -24,6 +24,7 @@ else:
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -68,7 +69,7 @@ ROOT_URLCONF = 'nextdoorcharity.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
