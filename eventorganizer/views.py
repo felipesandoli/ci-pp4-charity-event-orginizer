7e76b9322044c446a968c276283a1f8f5538da25
felipesandoli <cfelipesandoli@gmail.com>
2023-04-16 12:33:51 +0100

    Add Sign Up functionality

--- eventorganizer/views.py
+++ eventorganizer/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views import View
-
+from django.views import View, generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 class HomePage(View):
 
     def get(self, request):
         return render(request, 'index.html')
+
+# Signup following learndjango tutorial  
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = "registration/signup.html"
