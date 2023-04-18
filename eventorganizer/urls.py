f93c557151b6a41e0433df0919df144447f10334
felipesandoli <cfelipesandoli@gmail.com>
2023-04-18 11:28:32 +0100

    Change logout view to class based

--- eventorganizer/urls.py
+++ eventorganizer/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.HomePage.as_view(), name='homepage'),
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('logout', views.logout_view, name='logout_view')
+    path('logout', views.Logout.as_view(), name='logout_view')
 ]
--- eventorganizer/views.py
+++ eventorganizer/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect
 from django.views import View, generic
 from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.messages import constant as messages
 from django.urls import reverse_lazy
+
 
 class HomePage(View):
 
     def get(self, request):
         return render(request, 'index.html')
+
 
 # Signup following learndjango tutorial  
 class SignUp(generic.CreateView):
@@ -15,7 +18,9 @@ class SignUp(generic.CreateView):
     success_url = reverse_lazy('login')
     template_name = "registration/signup.html"
 
-#function based view for logout
-def logout_view(request):
-    logout(request)
-    return redirect("homepage")
+
+class Logout(View):
+
+    def get(self, request):
+        logout(request)
+        return redirect("homepage")
