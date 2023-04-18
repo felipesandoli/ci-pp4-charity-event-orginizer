bddb8d43f7446a2363bf608ce782b79f1009abb6
felipesandoli <cfelipesandoli@gmail.com>
2023-04-18 11:15:18 +0100

    Change logout redirect to homepage

--- eventorganizer/urls.py
+++ eventorganizer/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='homepage'),
-    path('signup/', views.SignUp.as_view(), name='signup')
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('logout', views.logout_view, name='logout_view')
 ]
--- eventorganizer/views.py
+++ eventorganizer/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View, generic
+from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
@@ -13,4 +14,8 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = "registration/signup.html"
-    
+
+#function based view for logout
+def logout_view(request):
+    logout(request)
+    return redirect("homepage")
