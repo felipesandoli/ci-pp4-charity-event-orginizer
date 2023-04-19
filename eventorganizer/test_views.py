ff19cf8a927a01c209370177e6821d1bd8a0d8a9
felipesandoli <cfelipesandoli@gmail.com>
2023-04-19 20:43:24 +0100

    Test homepage view

---
+++ eventorganizer/test_views.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+class TestViews(TestCase):
+
+    # test homepage following hello django project
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
