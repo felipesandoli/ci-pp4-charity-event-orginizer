c2fe4f7489d1c02ab541f4301ebfd23c8fe5fba0
felipesandoli <cfelipesandoli@gmail.com>
2023-04-22 19:06:13 +0100

    Change location field path to staticfiles storage

--- nextdoorcharity/settings.py
+++ nextdoorcharity/settings.py
@@ -139,7 +139,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-LOCATION_FIELD_PATH = STATIC_ROOT
+LOCATION_FIELD_PATH = STATICFILES_STORAGE
 
 LOCATION_FIELD = {
     'provider.google.api': '//maps.google.com/maps/api/js?sensor=false',
