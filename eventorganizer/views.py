fd4c542d57aebd5f45c81ffd6ad66ae2f5241c9a
felipesandoli <cfelipesandoli@gmail.com>
2023-05-06 14:03:28 +0100

    Add leave event button for cancelling participation in an event

--- eventorganizer/views.py
+++ eventorganizer/views.py
@@ -79,13 +79,17 @@ class EventInformation(View):
 
     def get(self, request, event_id):
         event = get_object_or_404(Event, id=event_id)
+        if request.user in event.participants.all():
+            user_participating = True
+        else:
+            user_participating = False
 
         if (
             request.user.is_authenticated
             and event.approved
             and not event.archived
         ):
-            return render(request, "event_information.html", {"event": event})
+            return render(request, "event_information.html", {"event": event, 'user_participating': user_participating})
         elif not request.user.is_authenticated:
             messages.add_message(
                 request,
