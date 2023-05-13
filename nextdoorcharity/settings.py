dd14c59e6b8846a6fc8b6f17ca4b8fa15f874d97
felipesandoli <cfelipesandoli@gmail.com>
2023-05-13 19:27:03 +0100

    Set debug accordingly to development and production

--- nextdoorcharity/settings.py
+++ nextdoorcharity/settings.py
@@ -36,7 +36,7 @@ LOGIN_REDIRECT_URL = '/'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
 if development:
     ALLOWED_HOSTS = ['127.0.0.1']
